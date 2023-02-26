f45c8097e5f63f31c01e9cae76cc63a1f8f0ee82
programmer_cj <programmer_cj@163.com>
2023-02-26 19:50:28 +0800

    init

---
+++ src/test/java/com/example/malllearn/MallLearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.malllearn;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallLearnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
